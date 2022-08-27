459fe1cd8af6a556ecd97281d226532eae798240
Thomas R <thomsirak@gmail.com>
2022-08-27 11:41:02 +0200

    endpoint for groceries

---
+++ Groceries/src/Groceries.Api/Features/GroceriesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Groceries.Api.Features
+{
+    [ApiController]
+    [Route("Groceries")]
+    public class GroceriesController : ControllerBase
+    {
+        public static List<Grocery> Groceries = new List<Grocery>()
+        {
+            new Grocery()
+            {
+                Name = "Carottes",
+                Id = 1
+            },
+            new Grocery()
+            {
+                Name = "Pommes",
+                Id = 2
+            }
+        };
+
+        [HttpGet]
+        [Route("")]
+        public List<Grocery> GetAll()
+        {
+ 
+
+            return Groceries;
+        }
+
+        [HttpGet]
+        [Route("{groceryId}")]
+        public Grocery GetOne(int groceryId)
+        {
+            var grocery = Groceries
+                .Where(g => g.Id == groceryId)
+                .FirstOrDefault();
+
+            return grocery;
+        }
+    }   
+}
---
+++ Groceries/src/Groceries.Api/Grocery.cs
@@ -0,0 +1,18 @@
+﻿namespace Groceries.Api
+{
+    public class Grocery
+    {
+        public Grocery()
+        {
+        }
+
+        public Grocery(string name)
+        {
+            Name = name;   
+        }
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+        
+    }
+}
